46f9087c4adec06fe0f23c8bd37c85a63d161df4
Qi Yuan Wong <s10222150@connect.np.edu.sg>
2023-04-21 21:49:25 +0800

    why java no "mode" function

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Question5
 {
@@ -26,7 +27,31 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
+    int[] arr={};
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int num = in.nextInt(); //read user input (int)
+    for (int i = 0; i<num; i++) {
+      arr = Arrays.copyOf(arr, arr.length + 1); //replace original array and add extra slot
+      arr[i]=in.nextInt();
+    }
     
+    //find mode in array
+    int maxValue = 0, maxCount = 0, i, j;
+
+      for (i = 0; i < num; ++i) {
+         int count = 0;
+         for (j = 0; j < num; ++j) {
+            if (arr[j] == arr[i])
+            ++count;
+         }
+
+         if (count > maxCount) {
+            maxCount = count;
+            maxValue = arr[i];
+         }
+      }
+
+    System.out.println("Mode: "+maxValue);
   }
 }
