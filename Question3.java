34fea203abb3827a6e08c54866f49b0d6c5556ec
Qi Yuan Wong <s10222150@connect.np.edu.sg>
2023-04-21 21:18:58 +0800

    Questions 1-4 !

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Enter a number: ");
+    int num = in.nextInt(); //read user input (int)
+    int numsqr = num*num;
+    System.out.println(numsqr);
   }
 }
